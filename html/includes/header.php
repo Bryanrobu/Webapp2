deb4ca90b253c8c3a276e7b09ece672d642931ff
Bryanrobu <1211898@student.roc-nijmegen.nl>
2025-05-21 09:40:09 +0200

    begin includes
    aanmaken includes voor header en footer

---
+++ html/includes/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>document</title>
+ 
+</head>
+<body>
+    <header>
+        <div class="header-sides-cont">
+            <img src="images/HorizonTravelsLogo.png" class="reisbureau-logo" alt="Reisbureau logo">
+            <a href="index.php" class="naam-doos">
+                <span class="naam-reisbureau">Horizon Travels</span>
+            </a>
+        </div>
+ 
+        <ul class="menu-bar">
+            <li>
+                <a href="index.php" class="menu-text">
+                    Home
+                </a>
+            </li>
+            <li><a href="overons.php" class="menu-text">
+                    Over ons
+                </a>
+            </li>
+            <li>
+                <a href="contact.php" class="menu-text">
+                    Contact
+                </a>
+            </li>
+            <li>
+                <a href="reizen.php" class="menu-text">
+                    Reizen
+                </a>
+            </li>
+        </ul>
+ 
+        <div class="header-sides-cont">
+           <div class="login">
+                <a href="login.php" class="black">Inloggen</a>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
