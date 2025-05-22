78859e63b7753474bb303753b0a2589039bb2be3
Sander227 <1211888@student.roc-nijmegen.nl>
2025-05-22 10:33:51 +0200

    toevoeging over-ons pagina

--- html/over-ons/index.php
+++ html/over-ons/index.php
@@ -1,12 +1,82 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
     <link rel="shortcut icon" href="../images/HorizonTravelsLogo.png" type="image/x-icon">  
     <title>Over ons</title>
 </head>
+
 <body>
     <?php include_once ('../includes/header.php'); ?>
+    <main>
+        <div class="over-ons-cont">
+            <h1>
+                Over ons
+            </h1>
+            <span>
+                Horizon Travels is jouw partner in onvergetelijke reiservaringen. Sinds 2010 brengen wij mensen naar de
+                mooiste bestemmingen wereldwijd, met oog voor cultuur, comfort en duurzaamheid.
+            </span>
+        </div>
+        <div class="over-ons-cont">
+            <h1>
+                Onze missie
+            </h1>
+            <span>
+                We willen mensen inspireren en verbinden door middel van betekenisvolle reizen. Elke klant verdient een
+                unieke, persoonlijke reiservaring.
+            </span>
+        </div>
+        <div class="over-ons-cont">
+            <h1>
+                Waarom kiezen voor ons?
+            </h1>
+            <span>
+                - Persoonlijke service op maat<br>
+                - Unieke en verrassende bestemmingen<br>
+                - 24/7 ondersteuning tijdens jouw reis<br>
+                - Duurzaam en verantwoord reizen
+            </span>
+        </div>
+        <div class="over-ons-cont">
+            <h1>
+                Ons team
+            </h1>
+            <span>
+                <table>
+                    <tr>
+                        <th>Naam</th>
+                        <th>Functie</th>
+                        <th>Favoriete bestemming</th>
+                    </tr>
+                    <tr>
+                        <td>Emma de Vries</td>
+                        <td>Reisadviseur</td>
+                        <td>Thailand</td>
+                    </tr>
+                    <tr>
+                        <td>Lars Jansen</td>
+                        <td>Klantenservice</td>
+                        <td>Costa Rica</td>
+                    </tr>
+                    <tr>
+                        <td>Fatima El Amrani</td>
+                        <td>Marketing en Social media</td>
+                        <td>Marokko</td>
+                    </tr>
+                    <tr>
+                        <td>Tom van Dijk</td>
+                        <td>Oprichter</td>
+                        <td>Nieuw-Zeeland</td>
+                    </tr>
+                </table>
+            </span>
+        </div>
+    </main>
+
 </body>
+
 </html>
