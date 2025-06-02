b95d0b8dbd9d77f38278958d98dde52c46e8e463
Sander227 <1211888@student.roc-nijmegen.nl>
2025-06-02 09:02:18 +0200

    Home pagina
    Begin van home pagina, zoekbalk toegevoegd en begin bestemming blokjes

--- html/index.php
+++ html/index.php
@@ -9,6 +9,30 @@
 </head>
 <body>
     <?php include_once ('includes/header.php'); ?>
+
+    <main>
+        <form class="home-cont center column">
+            <input type="text" id="destination" name="destination" class="zoek-balk" placeholder="Bestemming zoeken">
+            <button type="submit" class="verzend-knop">Zoeken</button>
+        </form>
+
+        <div class="populaire-bestemmingen-cont center column">
+            <div class="populaire-bestemmingen-blokje">
+                
+            </div>
+            <div class="populaire-bestemmingen-blokje">
+                
+            </div>
+            <div class="populaire-bestemmingen-blokje">
+                
+            </div>
+            
+        </div>
+
+        <div class="welkom-text">
+            
+        </div>
+    </main>
     <?php include_once ('includes/footer.php'); ?>
 </body>
 </html>
