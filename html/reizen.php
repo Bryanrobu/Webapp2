7ae846f10099dd0361faeae2bada164377290313
Bryanrobu <1211898@student.roc-nijmegen.nl>
2025-06-04 15:04:35 +0200

    login systeem aangepast
    hij onthoudt nu of je bent ingelogd, je id en of je een admin bent of niet

---
+++ html/reizen.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+$is_logged_in = isset($_SESSION["user"]);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="shortcut icon" href="../images/HorizonTravelsLogo.png" type="image/x-icon">  
+    <title>Reizen</title>
+</head>
+<body>
+    <?php include_once ('includes/header.php'); ?>
+    <?php include_once ('includes/footer.php'); ?>
+</body>
+</html>
